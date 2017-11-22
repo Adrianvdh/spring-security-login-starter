ca75537e606faf6f3e97bd3edc3d6d0ea3f998cf
Adrian van den Houten <adrian@zailab.com>
2017-11-22 15:21:21 +0200

    Init

---
+++ src/main/java/com/loginstarter/LoginStarterApplication.java
@@ -0,0 +1,12 @@
+package com.loginstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginStarterApplication.class, args);
+	}
+}
---
+++ src/main/java/com/loginstarter/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.loginstarter.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf()
+                .disable()
+            .authorizeRequests()
+                .anyRequest().authenticated()
+            .and()
+                .formLogin()
+                .and()
+                .logout();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+}
