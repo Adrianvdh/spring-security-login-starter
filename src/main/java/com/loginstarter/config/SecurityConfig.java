88ca4ff3811feed533ce3464df95fe4376e671d5
Adrian van den Houten <adrianvdh@gmail.com>
2017-11-22 20:39:27 +0200

    Added more onto spring security

--- src/main/java/com/loginstarter/config/SecurityConfig.java
+++ src/main/java/com/loginstarter/config/SecurityConfig.java
@@ -1,24 +1,31 @@
 package com.loginstarter.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
+//@EnableGlobalMethodSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .csrf()
-                .disable()
+        .and()
+            .rememberMe().rememberMeCookieName("remember-me")
+        .and()
+            .httpBasic()
+        .and()
             .authorizeRequests()
-                .anyRequest().authenticated()
-            .and()
-                .rememberMe().rememberMeCookieName("remember-me")
-            .and()
-                .httpBasic();
+            .anyRequest().authenticated();
     }
 
 
@@ -27,5 +34,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //auth.
         auth.inMemoryAuthentication()
                 .withUser("admin").password("password").roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/loginstarter/security/DefaultUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.loginstarter.security;
+
+import com.loginstarter.model.User;
+import com.loginstarter.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Component("userDetailsService")
+public class DefaultUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public DefaultUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> storedUser = userRepository.findOneByUsername(username);
+
+        return storedUser.map(user -> {
+            return new org.springframework.security.core.userdetails.User(
+                    username,
+                    user.getPassword(),
+                    Collections.singleton(() -> "ROLE_ADMIN"));
+        }).orElseThrow(() -> new RuntimeException("User " + username + " was nt found!"));
+    }
+}
