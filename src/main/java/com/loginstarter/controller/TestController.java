96a18c226ffdfcb3d23b3da0d056e5f07492ec6f
Adrian van den Houten <adrianvdh@gmail.com>
2017-11-23 06:56:33 +0200

    Returns string now instead of marshalled object

--- src/main/java/com/loginstarter/controller/TestController.java
+++ src/main/java/com/loginstarter/controller/TestController.java
@@ -1,16 +1,17 @@
 package com.loginstarter.controller;
 
-import com.loginstarter.user.model.UserVM;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/hello")
 public class TestController {
 
     @GetMapping
-    public UserVM protectedPage() {
-        return new UserVM("adrian", "adrian@zailab.com", "1123");
+    public String protectedPage(HttpServletRequest request) {
+        return "Welcome !" + request.getRemoteUser();
     }
 }
